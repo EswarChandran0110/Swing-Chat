7a868636eb07134932f1f1ce8b683a0651acf22a
Eswar_Chandran <eswarchandran0110@gmail.com>
2025-01-17 11:38:56 +0530

    protected user search api

--- back-end/Routes/userRoutes.js
+++ back-end/Routes/userRoutes.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const { registerUser, authUser } = require("../Controllers/userController");
+const {
+  registerUser,
+  authUser,
+  allUsers,
+} = require("../Controllers/userController");
+const { protected } = require("../Middlewares/authMiddleware");
 
 const router = express.Router();
 
+router.route("/").get(protected, allUsers);
 router.route("/register").post(registerUser);
 router.post("/login", authUser);
 module.exports = router;
